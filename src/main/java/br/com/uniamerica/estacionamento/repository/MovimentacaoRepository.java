ab091a24cbdcd042a4365a427ce1ed22918b1fb6
tokram <xtokram@gmail.com>
2023-05-21 11:21:43 -0300

    Class Repository Movimentacao

--- src/main/java/br/com/uniamerica/estacionamento/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/MovimentacaoRepository.java
@@ -1,8 +1,9 @@
 package br.com.uniamerica.estacionamento.repository;
 
+import br.com.uniamerica.estacionamento.entity.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovimentacaoRepository extends JpaRepository {
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
 }
