d2a5586f99a01e2c4cc60cd7a55ce951304cfa0f
tokram <xtokram@gmail.com>
2023-05-21 11:21:52 -0300

    VeiculoRepository

--- src/main/java/br/com/uniamerica/estacionamento/repository/VeiculoRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/VeiculoRepository.java
@@ -1,9 +1,30 @@
 package br.com.uniamerica.estacionamento.repository;
 
 import br.com.uniamerica.estacionamento.entity.Modelo;
+import br.com.uniamerica.estacionamento.entity.Movimentacao;
+import br.com.uniamerica.estacionamento.entity.Tipo;
+import br.com.uniamerica.estacionamento.entity.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Repository
 public interface VeiculoRepository extends JpaRepository<Modelo, Long> {
+    @Query("from Movimentacao where Veiculo = :veiculo")
+    public List<Movimentacao> findMovimentacaoByVeiculo(@Param("veiculo") final Veiculo veiculo);
+
+    @Query("from Veiculo where id = :modelo")
+    public List<Veiculo> findModelo(@RequestParam("modelo") final Modelo modelo);
+
+    @Query("from Veiculo where placa = :placa")
+    public List<Veiculo> findPlaca(@RequestParam("placa") final String placa);
+
+    @Query("from Veiculo where tipo = :tipo")
+    public List<Veiculo> findTipo(@RequestParam("tipo") final Tipo tipo);
 }
+
+
