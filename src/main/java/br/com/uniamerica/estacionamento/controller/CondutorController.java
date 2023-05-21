e6007b52d77a03a0a20f33fb530498aefc7fc1eb
tokram <xtokram@gmail.com>
2023-05-21 11:20:14 -0300

    Controller OK

--- src/main/java/br/com/uniamerica/estacionamento/controller/CondutorController.java
+++ src/main/java/br/com/uniamerica/estacionamento/controller/CondutorController.java
@@ -3,16 +3,90 @@ package br.com.uniamerica.estacionamento.controller;
 
 import br.com.uniamerica.estacionamento.entity.Condutor;
 import br.com.uniamerica.estacionamento.repository.CondutorRepository;
+import br.com.uniamerica.estacionamento.service.CondutorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects.*;
 
 @Controller
-@RequestMapping("/api/condutores")
+@RequestMapping(value = "/api/condutor")
 public class CondutorController {
+
     @Autowired
-    private CondutorRepository condutorRepository;
+    public CondutorRepository condutorRepository;
+
+    @Autowired
+    public CondutorService condutorService;
+
+
+    @GetMapping
+    public ResponseEntity<?> findByIdParam(@RequestParam("id") final Long id){
+        final Condutor condutor = this.condutorRepository.findById(id).orElse(null);
+
+        return condutor == null
+                ? ResponseEntity.badRequest().body("Condutor não encontrado!")
+                : ResponseEntity.ok(condutor);
+    }
+
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> listaCompleta(){
+
+        return ResponseEntity.ok(this.condutorRepository.findAll());
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<?> cadastrar (@RequestBody final Condutor condutor) {
+
+        try{
+            this.condutorService.cadastrar(condutor);
+            return ResponseEntity.ok("Regitro realizado com sucesso.");
+        } catch (DataIntegrityViolationException erro){ //erro de violação de integridade de dados
+            return ResponseEntity.internalServerError().body("Erro"+erro.getMessage());
+        } catch (RuntimeException erro){ //erro de varais exceções de tempo de execução
+            return ResponseEntity.internalServerError().body("Erro"+erro.getMessage());
+        } catch (Exception erro){ // Se ocorrer outra exceção nao capturada pelos blocos anteriores
+            return ResponseEntity.badRequest().body("Erro"+erro.getMessage());
+        }
+
+    }
+
+
+
+    @PutMapping
+    public ResponseEntity<?> editar (@RequestParam("id") final Long id, @RequestBody final Condutor condutor){
+        try{
+            final Condutor condutorData = this.condutorRepository.findById(id).orElse(null);
+
+            if(condutorData == null || !condutorData.getId().equals(condutor.getId())){
+                throw new RuntimeException("Registro não identificado");
+            }
+
+            this.condutorRepository.save(condutor);
+            return ResponseEntity.ok("Registro atualizado");
+        }catch (DataIntegrityViolationException ex){
+            return ResponseEntity.internalServerError().body("Error"+ex.getCause().getMessage());
+        }catch (RuntimeException ex){
+            return ResponseEntity.internalServerError().body("Erro"+ex.getMessage());
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete (@RequestParam("id") final Long id){
+        final Condutor condutorData = this.condutorRepository.findById(id).orElse(null);
+
+        try{
+            this.condutorService.deletar(condutorData);
+            return ResponseEntity.ok("Registro deletado");
+        }catch (RuntimeException erro){
+            return ResponseEntity.internalServerError().body("Erro"+erro.getMessage());
+        }
+    }
 }
